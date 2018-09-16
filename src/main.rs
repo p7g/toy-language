6ececdefab02fcaecc28a3217314b0dcf6927550
Patrick Gingras <ging0044@algonquinlive.com>
2018-09-16 19:52:45 -0400

    remove useless example code

--- src/main.rs
+++ src/main.rs
@@ -17,29 +17,6 @@ fib = fn (n) if n < 2 then n else fib(n - 1) + fib(n - 2);
 
 println(fib(15));
 
-print-range = fn(a, b)             # `λ` is synonym to `lambda`
-                if a <= b then {  # `then` here is optional as you can see below
-                  print(a);
-                  if a + 1 <= b then {
-                    print(", ");
-                    print-range(a + 1, b);
-                  } else println("");        # newline
-                };
-print-range(1, 5);
-"#.to_string();
-
-    let code2 = &r#"
-# this is a comment
-
-println("Hello World!");
-
-println(2 + 3 * 4);
-
-# functions are introduced with `lambda` or `λ`
-fib = fn (n) if n < 2 then n else fib(n - 1) + fib(n - 2);
-
-println(fib(15));
-
 print_range = fn(a, b)             # `λ` is synonym to `lambda`
                 if a <= b then {  # `then` here is optional as you can see below
                   print(a);
@@ -51,7 +28,7 @@ print_range = fn(a, b)             # `λ` is synonym to `lambda`
 print_range(1, 5);
 "#.to_string();
 
-    let input_stream = InputStream::new(code2);
+    let input_stream = InputStream::new(code);
     let lexer = TokenStream::new(input_stream);
     let mut parser = Parser::new(lexer);
 
